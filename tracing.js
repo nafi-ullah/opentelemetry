30b888aabb4f7babc76a18becb9502a50cf7c321
nafi-ullah <shafinnafiullah@gmail.com>
2024-02-22 13:47:19 +0600

    full setup with zipkin

--- tracing.js
+++ tracing.js
@@ -2,9 +2,19 @@
 
 const { LogLevel } = require("@opentelemetry/core");
 const { NodeTracerProvider } = require("@opentelemetry/node");
+const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-node");
+const { ZipkinExporter } = require("@opentelemetry/exporter-zipkin");
 
 const provider = new NodeTracerProvider({
     logLevel: LogLevel.ERROR
 });
 
 provider.register();
+
+provider.addSpanProcessor(
+    new SimpleSpanProcessor(
+        new ZipkinExporter({
+            serviceName: 'getting-started'
+        })
+    )
+)
